020cf41e9578829b34252af3eb95537e50ee53c5
luncenok <idziejczak3@gmail.com>
2021-10-01 15:42:32 +0200

    Add animation

--- src/components/Statistic.js
+++ src/components/Statistic.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Statistic = ({name, icon, progress}) => {
+const Statistic = ({name, icon, progress, onClick}) => {
     return (
-        <div className='statistic'>
+        <div className='statistic' onClick={onClick}>
             <div className='progress' style={{
                 height: 1 + progress * 79,
                 backgroundColor: ((progress < 0.1) || (progress > 0.9)) ? '#836333' : '#856c4d'
@@ -20,6 +20,7 @@ Statistic.propTypes = {
     name: PropTypes.string,
     icon: PropTypes.element,
     progress: PropTypes.number,
+    onClick: PropTypes.func,
 };
 
 Statistic.defaultProps = {
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, {useState} from 'react';
 import Statistic from "./Statistic";
 import SupervisorAccountIconOutlined from '@mui/icons-material/SupervisorAccountOutlined';
 import EmojiPeopleIconOutlined from '@mui/icons-material/EmojiPeopleOutlined';
@@ -7,18 +6,46 @@ import CalendarTodayIconOutlined from '@mui/icons-material/CalendarTodayOutlined
 import EmojiEmotionsOutlinedIcon from '@mui/icons-material/EmojiEmotionsOutlined';
 
 const Statistics = ({props}) => {
+    const [teacherProgress, setTeacherProgress] = useState(0.05)
+    const [studentProgress, setStudentProgress] = useState(0.7)
+    const [attendanceProgress, setAttendanceProgress] = useState(0.95)
+    const [comfortProgress, setComfortProgress] = useState(0.3)
     return (
         <div className='statistics'>
-            <Statistic name='teacher' icon={<SupervisorAccountIconOutlined />} progress={0.95}/>
-            <Statistic name='student' icon={<EmojiPeopleIconOutlined />} progress={0.7}/>
-            <Statistic name='attendance' icon={<CalendarTodayIconOutlined />} progress={0.05}/>
-            <Statistic name='comfort' icon={<EmojiEmotionsOutlinedIcon />} progress={0.3}/>
+
+            <Statistic
+                name='teacher'
+                icon={<SupervisorAccountIconOutlined/>}
+                progress={teacherProgress}
+                onClick={() => {
+                    setTeacherProgress(0.3)
+                }}
+            />
+            <Statistic
+                name='student'
+                icon={<EmojiPeopleIconOutlined/>}
+                progress={studentProgress}
+                onClick={() => {
+                    setStudentProgress(1.00)
+                }}/>
+            <Statistic
+                name='attendance'
+                icon={<CalendarTodayIconOutlined/>}
+                progress={attendanceProgress}
+                onClick={() => {
+                    setAttendanceProgress(0.7)
+                }}/>
+            <Statistic
+                name='comfort'
+                icon={<EmojiEmotionsOutlinedIcon/>}
+                progress={comfortProgress}
+                onClick={() => {
+                    setComfortProgress(0.05)
+                }}/>
         </div>
     );
 };
 
-Statistics.propTypes = {
-
-};
+Statistics.propTypes = {};
 
 export default Statistics;
